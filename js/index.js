065b46190f6f253a82b517ed202363a672f16e75
Pablo <121295808+polargentino@users.noreply.github.com>
2023-03-20 07:21:50 -0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+document.getElementById("boton").addEventListener("click", function() {
+
+    document.getElementById("demo").innerHTML = "Estamos probando eventos js";
+    })
+    
+    document.getElementById("boton_color").addEventListener("click", function() {
+    document.body.style.background = "rgb(203, 241, 228)";
+    })
+    
+    document.getElementById("boton_default").addEventListener("click", function() {
+    document.body.style.background = " rgb(245, 242, 59)";
+     })  
+    
+     document.getElementById("boton_ocultar").addEventListener("click", function() {
+    
+     document.getElementById("demo").style.display = "none";
+     })
+    
+     alert("Este es mi primer script, presione aceptar para ver el trabajo final, estamos aprendiendo lo maxímo posible para aprobar; le dediqué muchas semanas y horas para llegar a este punto, agradecería mucho un WhatsApp con un si o no, 3424366231, desde ya muchas gracias!!")
+    
+     
+     
